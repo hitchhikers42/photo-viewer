e660e1aa40e892f4e0d33e08fc024c951cd62679
ptbarnum4 <peterbarnum@gmail.com>
2019-12-02 23:30:36 -0700

    Cleamed up

--- database/db.js
+++ database/db.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql');
 
 const cx = mysql.createConnection({
+  host: 'localhost',
   user: 'root',
   password: '',
   database: 'fec_images'
 });
 
 cx.connect(err => {
-  err ? console.error('ERROR CONNECTING TO DATABASE `FEC_IMAGES`')
-  : console.log('Successfully connected to FEC_IMAGES')
+  if(err) {
+    console.error('ERROR CONNECTING TO DATABASE `FEC_IMAGES`')
+
+  } else {
+    console.log('Successfully connected to FEC_IMAGES')
+  }
 });
 
 module.exports = cx;
