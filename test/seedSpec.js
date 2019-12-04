fd1e624ee4f728c17aea1fd75922ad4fce8e853d
ptbarnum4 <peterbarnum@gmail.com>
2019-12-04 11:38:22 -0700

    Working on tests for seed script

---
+++ test/seedSpec.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+const mysql = require('mysql');
+
+
+
+
+beforeEach('Connect to the database', function() {
+  db = mysql.createConnection({
+    user: 'root',
+    password: '',
+    database: 'fec_images'
+  });
+  db.connect(function(err) {
+    if (err) { return done(err); }
+  });
+
+})
+
+
+describe('Testing the test', function() {
+
+  it('Expect 12 entries in "img" table', function(done) {
+    var q = 'SELECT * FROM img';
+    db.query(q, (err, results) => {
+      if(err) {
+        console.error(err);
+        done(err)
+      } else {
+        expect(results.length).to.equal(12)
+        done()
+      }
+    })
+
+  })
+  it('Expect 4 entries in "img" table with productId = "BES870XL"', function(done) {
+    var q = 'SELECT * FROM img WHERE productId=?';
+    db.query(q, ['BES870XL'], (results) => {
+      expect(results.length).to.equal(4)
+    })
+
+  })
+
+
+})
+
+
+/*
+    'BES870XL',
+      'IVFWCT242DBWH',
+      'TOB-135N'
+*/
