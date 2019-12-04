3c79209972a6bb0bf9b799bb9d51b2035343754c
ptbarnum4 <peterbarnum@gmail.com>
2019-12-04 11:40:01 -0700

    Refactored seed

--- database/seed.js
+++ database/seed.js
@@ -13,34 +13,49 @@ AWS.config.loadFromPath('./config/aws.json')
 /* Create AWS S3 Instance */
 const s3 = new AWS.S3()
 
-const fetchObjects = (callback) => {
+const fetchS3Bucket = (callback) => {
   // List all objects in s3 mockbbb bucket
   s3.listObjects({ Bucket: 'mockbbb' }, (err, data) => {
     err ? callback(err)
     : callback(null, data)
   })
 }
-fetchObjects((err, data) => {
-  if (err) {
-    console.error(err)
-  } else {
-    const bucket = data.Name;
-    data.Contents.forEach(image => {
-      var keyElements = image.Key.split('/');
-      var imgKey = keyElements.pop()
-      var productId = keyElements.join('/')
-      var title = Data[productId].title;
-      var params = [imgKey, productId, bucket, title];
-      db.query(
-        'INSERT INTO img VALUES(?, ?, ?, ?)',
-        params,
-        (err, result) => {
-          err ?
-            console.error(err) :
-            console.log(`Successfully added ${params.join(' ')} to database`);
-        });
+
+const seed = ({ Name, Contents }) => {
+    Contents.forEach((image, i) => {
+      let elements = image.Key.split('/');
+      let imgKey = elements.pop();
+      let productId = elements.join('/');
+      let title = Data[productId].title;
+
+      if(imgKey !== '') {
+        db.query(
+          'INSERT INTO img VALUES(?, ?, ?, ?)',
+          [imgKey, productId, Name, title],
+          (err, result) => {
+            err ?
+              console.error(err) :
+              console.log(`
+                Successfully added:
+                  ImageKey: ${imgKey}
+                  Product ID: ${productId}
+                  Bucket Name: ${Name}
+                  Title/Description: ${title}
+               to 'img' Table in 'fec_images' Database
+               Item Count = ${i+1}
+               ******************************************`);
+          }
+        );
+      }
     });
     // End the db connection to free up the console
     db.end()
+}
+fetchS3Bucket((err, data) => {
+  if (err) {
+    console.error(err)
+  } else {
+    seed(data)
   }
 })
+module.exports = { seed, fetchS3Bucket }
